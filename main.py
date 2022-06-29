d6ebd35090cf66d7b3c6811433e0be486f7babd1
Lee <leeuklee@gmail.com>
2022-06-29 14:00:08 +0200

    added waveforms

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+
 from machine import I2C, Pin
 
 import mcp4725
@@ -8,6 +9,31 @@ i2c.scan()
 
 dac = mcp4725.MCP4725(i2c, mcp4725.BUS_ADDRESS[0])
 
+# sine wave
 while True:
     for i in range(4095, 0, -50):
-        dac.write(1400 + int(1240 * (math.sin(2 * math.pi * i / 4095))))
+        dac.write(500 + int(500 * (math.sin(2 * math.pi * i / 4095))))
+
+# square wave
+while True:
+    dac.write(1240)
+    sleep(0.5)
+    dac.write(0)
+    sleep(0.5)
+
+# Sawtooth wave
+start = 0
+max = 1040
+value = 0
+step = 100
+sleep_time = 0.01
+
+while True:
+    while value < max:
+        value = value + step
+        dac.write(value)
+        sleep(sleep_time)
+    while value > start:
+        dac.write(value)
+        value = value - step
+        sleep(sleep_time)
